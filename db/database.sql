3c99930bf9db4c229c72689bb04feabc4b9412f4
Dale Tang <ss9430881@gmail.com>
2025-12-31 13:28:35 +0000

    final

--- db/database.sql
+++ db/database.sql
@@ -12,36 +12,36 @@ DROP TABLE IF EXISTS `mobilepost`;
 CREATE TABLE `mobilepost` (
   `id`              BIGINT NOT NULL AUTO_INCREMENT,
 
-  -- Core Identifiers
+
   `mobileCode`      VARCHAR(10)  NOT NULL COMMENT 'Unique code for the mobile vehicle',
   `dayOfWeekCode`   TINYINT UNSIGNED NOT NULL COMMENT '1=Monday, 7=Sunday',
   `seq`             INT NOT NULL COMMENT 'Sequence number for the day',
 
-  -- English Data
+ 
   `nameEN`          VARCHAR(100) NOT NULL,
   `districtEN`      VARCHAR(100) NOT NULL,
   `locationEN`      VARCHAR(150) NOT NULL,
   `addressEN`       VARCHAR(255) NOT NULL,
 
-  -- Traditional Chinese Data
+ 
   `nameTC`          VARCHAR(100) NOT NULL,
   `districtTC`      VARCHAR(100) NOT NULL,
   `locationTC`      VARCHAR(150) NOT NULL,
   `addressTC`       VARCHAR(255) NOT NULL,
 
-  -- Simplified Chinese Data
+  
   `nameSC`          VARCHAR(100) NOT NULL,
   `districtSC`      VARCHAR(100) NOT NULL,
   `locationSC`      VARCHAR(150) NOT NULL,
   `addressSC`       VARCHAR(255) NOT NULL,
 
-  -- Time & Location
+  
   `openHour`        TIME NOT NULL,
   `closeHour`       TIME NOT NULL,
   `latitude`        DECIMAL(10,6) NOT NULL,
   `longitude`       DECIMAL(10,6) NOT NULL,
 
-  -- Constraints
+
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_mobile_day_seq` (`mobileCode`, `dayOfWeekCode`, `seq`),
   KEY `idx_districtEN` (`districtEN`),
